a8b8cff3146f68878eb1c78ece979f72cdf613c5
D-Crypt <danielvaughan54@gmail.com>
2021-05-11 16:05:55 +0100

    Define basic mathematical functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    if (operator = '+') {
+        add(x, y);
+    } else if (operator = '-') {
+        subtract(x, y);
+    } else if (operator = '*') {
+        multiply(x, y);
+    } else if (operator = '/') {
+        divide(x, y);
+    } 
+}
