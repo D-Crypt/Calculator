5b2f23d729a96e85009e9ddb3b6440932aed5ddb
D-Crypt <danielvaughan54@gmail.com>
2021-05-20 17:46:17 +0100

    Store x correctly (y branch not reached)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const display = document.querySelector(".display");
 const digitBtns = document.querySelectorAll(".digit");
-const acBtn = document.querySelector(".AC");
+const acBtn = document.querySelector(".clear");
 const operatorBtns = document.querySelectorAll(".operator");
 
 function add(x, y) {
@@ -21,7 +21,7 @@ function divide(x, y) {
 }
 
 function operate(operator, x, y) {
-    switch (operator.dataset.value) {
+    switch (operator) {
         case "+":
             return add(x, y);
         case "-":
@@ -35,30 +35,55 @@ function operate(operator, x, y) {
 }
 
 function changeDisplayValue() {
-    let currentValue = 0;
+    let currentValueX = 0;
+    let currentValueY;
     let operatorPressed = false;
+    let firstOperator;
     let currentOperator;
 
     digitBtns.forEach(button => {
         button.addEventListener("click", () => {
-            if (display.textContent == 0 || operatorPressed === true) {
+            if (display.textContent == 0 || operatorPressed === true) { 
                 display.textContent = button.dataset.value;
                 operatorPressed = false;
             } else display.textContent = display.textContent + button.dataset.value;
         })
     });
 
-    acBtn.addEventListener("click", () => {
-        display.textContent = 0;
-    })
-
     operatorBtns.forEach(button => {
         button.addEventListener("click", () => {
-            currentValue = display.textContent;
-            operatorPressed = true;
             currentOperator = button.dataset.value;
+
+            if (operatorPressed === false) {
+                currentValueX = display.textContent;
+                operatorPressed = true;
+                firstOperator = currentOperator;
+
+                console.log("X = " + currentValueX);
+            } else {
+                currentValueY = display.textContent;
+                display.textContent = operate(firstOperator, +currentValueX, +currentValueY);
+
+                console.log("Y = " + currentValueY);
+            }
         })
+    })
+
+    acBtn.addEventListener("click", () => {
+        currentValueX = 0;
+        currentValueY = 0;
+        display.textContent = currentValueX;
     })
 }
 
 changeDisplayValue()
+
+/* 
+
+Type numbers. 
+As soon as you press any operator, remember whatever is on the display (currentValueX).
+Now that you've pressed an operator, type out the other digits.
+As soon as you press any other operator for a second time, remember the display again (currentValueY)
+Perform the operate using the first operator, currentValueX and currentValueY
+
+*/ 
