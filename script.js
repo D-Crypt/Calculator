7b5a9b7d0ab57a1f6775d7b50cadf97b8e88bef3
D-Crypt <danielvaughan54@gmail.com>
2021-05-24 21:44:37 +0100

    Fix calculate() logic (fix operator stacking)

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function operate(operator, x, y) {
             return multiply(x, y);
         case "/":
             if (x === 0) return null;
-            else return divide(x, y);
+            return divide(x, y);
     }
 }
 
@@ -42,27 +42,22 @@ function calculate() {
             if (display.textContent == 0 || isOperatorPressed === true) {
                 display.textContent = button.dataset.value;
                 isOperatorPressed = false;
-            }
-            else display.textContent = display.textContent + button.dataset.value;
+            } else display.textContent += button.dataset.value;
         })
     });
 
     operatorBtns.forEach(button => {
         button.addEventListener("click", () => {
-            operatorPressedTotal++;
-            currentOperator = button.dataset.value;
             isOperatorPressed = true;
 
-            if (operatorPressedTotal === 1) {
+            if (currentValueX === null) currentValueX = display.textContent;
+            else {
+                currentValueY = display.textContent;
+                display.textContent = operate(currentOperator, +currentValueX, +currentValueY);
                 currentValueX = display.textContent;
-                firstOperator = currentOperator;
             }
 
-            if (operatorPressedTotal === 2) {
-                currentValueY = display.textContent;
-                display.textContent = operate(firstOperator, +currentValueX, +currentValueY);
-                --operatorPressedTotal;
-            }
+            currentOperator = button.dataset.value;
         })
     })
 
@@ -73,12 +68,10 @@ function calculate() {
 
 function initialiseCalculator() {
     display.textContent = 0;
-    currentValueX = 0;
-    currentValueY = 0;
-    firstOperator = null;
+    currentValueX = null;
+    currentValueY = null;
     currentOperator = null;
     isOperatorPressed = false;
-    operatorPressedTotal = 0;
 }
 
 calculate();
@@ -88,5 +81,6 @@ console.log(operate("+", 1111, 111))
 /*
 
 Need to assess the previous expression (firstOperator and currentValueY) when pressing equals.
+Pressing any operator (instead of =) multiple times should not stack.
 
 */
